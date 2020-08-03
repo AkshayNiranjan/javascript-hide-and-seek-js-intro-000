137553f3e4ab5c251707bfd3f646cc8bca907d57
Akshay Niranjan <anstmfriend@gmail.com>
2020-08-03 22:38:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getFirstSelector(selector){
   return document.querySelector(selector);
 }
 function deepestChild(){
-  var start=document.getElementById("div.grand-node");
+  var start=document.querySelector("div.grand-node");
   while(start.querySelector("div")!=null){
     start=start.querySelector("div");
   }
